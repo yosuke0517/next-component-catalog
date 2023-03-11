5fbc6facd815fb372e1443dbc4266e7374dc9b41
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-03-11 15:08:58 +0900

    fix nextAuthでのメール認証を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 // src/pages/_app.tsx
 import "../styles/globals.css";
 import type { AppType } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
+import { Session } from "next-auth";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+import NextAuth, { DefaultSession, type NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "~/server/db/client";
+import { env } from "~/env/server.mjs";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  callbacks: {
+    session({ session, user }) {
+      if (session.user.id) {
+        session.user.id = user.id;
+        // session.user.role = user.role; <-- put other properties on the session here
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
