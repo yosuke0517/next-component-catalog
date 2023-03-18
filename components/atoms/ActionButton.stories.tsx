40bf5a988a3785dda017e768e799c2bfcefda613
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-03-18 16:34:52 +0900

    add ボタンコンポーネントの新規作成

---
+++ components/atoms/ActionButton.stories.tsx
@@ -0,0 +1,87 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MouseEventHandler, ReactNode } from "react";
+import { ActionButton, ActionButtonType } from "./ActionButton";
+// @ts-ignore // mdxでドキュメントも作成することができる（今回はパス）
+// import MDXDocument from "./AttachableIconButton.mdx";
+
+export default {
+  title: "ActionButton",
+  component: ActionButton,
+  argTypes: {
+    // propsに渡すvariantをStorybookから変更できるように追加
+    type: {
+      // ラジオボタンで設定できるように指定
+      control: { type: "radio" },
+      options: ["main", "sub"],
+    },
+    // propsに渡すchildrenをStorybookから変更できるように追加
+    children: {
+      // テキストボックスで入力できるように指定
+      control: { type: "text" },
+    },
+  },
+  parameters: {
+    docs: {
+      // ドキュメント用のmdxコンポーネントを指定
+      // page: MDXDocument,
+    },
+  },
+} as ComponentMeta<typeof ActionButton>;
+
+const callback = () => {
+  alert("callback!!");
+};
+
+export const Main = (
+  props: JSX.IntrinsicAttributes & {
+    type: ActionButtonType;
+    callback?: MouseEventHandler<HTMLButtonElement> | undefined;
+    children: ReactNode;
+  }
+) => {
+  return (
+    <ActionButton {...props} type="main" callback={callback}>
+      <span>要素を挿入することもできる🛩</span>
+    </ActionButton>
+  );
+};
+
+export const Sub = (
+  props: JSX.IntrinsicAttributes & {
+    type: ActionButtonType;
+    callback?: MouseEventHandler<HTMLButtonElement> | undefined;
+    children: ReactNode;
+  }
+) => {
+  return (
+    <ActionButton {...props} type="sub" callback={callback}></ActionButton>
+  );
+};
+
+export const Cancel = (
+  props: JSX.IntrinsicAttributes & {
+    type: ActionButtonType;
+    callback?: MouseEventHandler<HTMLButtonElement> | undefined;
+    children: ReactNode;
+  }
+) => {
+  return (
+    <ActionButton {...props} type="cancel" callback={callback}></ActionButton>
+  );
+};
+
+// テンプレートコンポーネントを実装
+// Storybookから渡されたpropsをそのままButtonに渡す
+const Template: ComponentStory<typeof ActionButton> = (args) => (
+  <ActionButton {...args} />
+);
+
+// bindを呼び出しStoryを作成
+export const TemplateTest = Template.bind({});
+
+// デフォルトのpropsを設定する
+TemplateTest.args = {
+  type: "main",
+  callback,
+  children: <span>画面上でPropsが触れるヤツ</span>,
+};
---
+++ components/atoms/ActionButton.tsx
@@ -0,0 +1,40 @@
+import { FC, MouseEventHandler, ReactNode, useState } from "react";
+import cn from "classnames";
+
+type ActionButtonProps = {
+  type: ActionButtonType;
+  callback?: () => void | Promise<void>;
+  children: ReactNode;
+};
+
+export type ActionButtonType = "main" | "sub" | "cancel";
+
+export const ActionButton: FC<ActionButtonProps> = ({
+  type = "main",
+  children,
+  callback,
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    if (callback) {
+      await callback();
+    }
+    setLoading(false);
+  };
+
+  const btnClass = cn("rounded-full text-white font-bold py-2 px-4", {
+    "bg-orange-500": type === "main",
+    "bg-blue-500": type === "sub",
+    "border border-orange-500 bg-white text-orange-400": type === "cancel",
+    "hover:opacity-80": true,
+  });
+
+  return (
+    <button className={btnClass} onClick={handleClick}>
+      {loading ? "Loading..." : ""}
+      {children ?? ""}
+    </button>
+  );
+};
