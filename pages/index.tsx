9f1763b9c5423d55b4af2bf9bcc0a6b7d88bc76d
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-03-21 18:09:12 +0900

    fix: テストデータ等微調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
-import type { NextPage } from "next";
-import { signOut, useSession } from "next-auth/react";
-import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
-import { Layout } from "~/components/Layout";
-import { Auth } from "~/components/Auth";
-import { ActionButton } from "~/components/atoms/ActionButton";
-import { SingleSelectInput } from "~/components/atoms/SingleSelectInput";
-import { MultiSelectInput } from "~/components/atoms/MultiSelectInput";
-import { XMarkIcon } from "@heroicons/react/20/solid";
-import React from "react";
+import type { NextPage } from 'next';
+import { signOut, useSession } from 'next-auth/react';
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid';
+import { Layout } from '~/components/Layout';
+import { Auth } from '~/components/Auth';
+import { ActionButton } from '~/components/atoms/ActionButton';
+import { SingleSelectInput } from '~/components/atoms/SingleSelectInput';
+import { MultiSelectInput } from '~/components/atoms/MultiSelectInput';
+import { XMarkIcon } from '@heroicons/react/20/solid';
+import React from 'react';
+import { TagList } from '~/components/atoms/TagList';
 
 // テストでーた TODO supabaseから取得するようにする
 const options = [
-  { id: 0, name: "Option 1" },
-  { id: 1, name: "Option 2" },
-  { id: 2, name: "Option 3" },
+  { id: 0, name: 'Option 1' },
+  { id: 1, name: 'Option 2' },
+  { id: 2, name: 'Option 3' },
+  { id: 3, name: 'Option 4' },
+  { id: 4, name: 'Option 5' },
+  { id: 5, name: 'Option 6' },
 ];
 
 const Home: NextPage = () => {
@@ -27,16 +31,31 @@ const Home: NextPage = () => {
   }
   return (
     <Layout title="Todo App">
-      <ArrowLeftOnRectangleIcon
-        className="h-6 w-6 cursor-pointer text-blue-600"
-        onClick={() => signOut()}
-      />
-      <p className="my-3 text-xl text-blue-600">{session?.user?.name}</p>
-      <ActionButton type="sub" callback={() => console.log(session)}>
-        hoge
-      </ActionButton>
-      <SingleSelectInput options={options} />
-      <MultiSelectInput options={options} />
+      <div className="flex">
+        <button type="button" onClick={() => signOut()}>
+          ログアウト
+        </button>
+        <ArrowLeftOnRectangleIcon className="h-6 w-6 cursor-pointer" />
+      </div>
+      <p className="my-3 text-xl text-blue-600">ID: {session?.user.email}</p>
+      <div>
+        <p>ActionButton</p>
+        <ActionButton type="sub" callback={() => console.log(session)}>
+          hoge
+        </ActionButton>
+      </div>
+      <div>
+        <p>SingleSelectInput</p>
+        <SingleSelectInput options={options} />
+      </div>
+      <div>
+        <p>MultiSelectInput</p>
+        <MultiSelectInput options={options} />
+      </div>
+      <div>
+        <p>TabList</p>
+        <TagList tagList={options} />
+      </div>
     </Layout>
   );
 };
