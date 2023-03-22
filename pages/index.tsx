d529629df92c2404dfdd950961f64d2e1e19e4af
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-03-22 22:58:11 +0900

    fix: 一旦認証のフォーム作成（TODOあとでコンポーネント化）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import type { NextPage } from 'next';
-import { signOut, useSession } from 'next-auth/react';
-import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid';
+import {
+  ArrowLeftOnRectangleIcon,
+  CheckBadgeIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/24/solid';
 import { Layout } from '~/components/Layout';
 import { Auth } from '~/components/Auth';
 import { ActionButton } from '~/components/atoms/ActionButton';
 import { SingleSelectInput } from '~/components/atoms/SingleSelectInput';
 import { MultiSelectInput } from '~/components/atoms/MultiSelectInput';
 import { XMarkIcon } from '@heroicons/react/20/solid';
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { TagList } from '~/components/atoms/TagList';
+import { supabase } from '~/utils/supabase';
+import { useMutateAuth } from '~/hooks/useMutateAuth';
 
 // テストでーた TODO supabaseから取得するようにする
 const options = [
@@ -21,11 +26,74 @@ const options = [
 ];
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  if (!session) {
+  const signOut = () => {
+    supabase.auth.signOut();
+  };
+  const user = supabase.auth.user();
+  const [isLogin, setIsLogin] = useState(true);
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loginMutation,
+    registerMutation,
+  } = useMutateAuth();
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLogin) {
+      loginMutation.mutate();
+    } else {
+      registerMutation.mutate();
+    }
+  };
+  if (!user) {
     return (
       <Layout title="Login">
-        <Auth />
+        <ShieldCheckIcon className="mb-6 h-12 w-12 text-blue-500" />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              required
+              className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              required
+              className="my-2 rounded border border-gray-300 px-3 py-2 text-sm  placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="my-6 flex items-center justify-center text-sm">
+            <span
+              onClick={() => setIsLogin(!isLogin)}
+              className="cursor-pointer font-medium hover:text-indigo-500"
+            >
+              change mode ?
+            </span>
+          </div>
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700"
+          >
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <CheckBadgeIcon className="h-5 w-5" />
+            </span>
+            {isLogin ? 'Login' : 'Register'}
+          </button>
+        </form>
       </Layout>
     );
   }
@@ -37,10 +105,10 @@ const Home: NextPage = () => {
         </button>
         <ArrowLeftOnRectangleIcon className="h-6 w-6 cursor-pointer" />
       </div>
-      <p className="my-3 text-xl text-blue-600">ID: {session?.user.email}</p>
+      <p className="my-3 text-xl text-blue-600">ID: {user.email}</p>
       <div>
         <p>ActionButton</p>
-        <ActionButton type="sub" callback={() => console.log(session)}>
+        <ActionButton type="sub" callback={() => console.log(user)}>
           hoge
         </ActionButton>
       </div>
