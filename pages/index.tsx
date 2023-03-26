d9e4d9327d1a9d5001b93f23df23608572393bcc
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-03-26 10:12:06 +0900

    fix: dummyデータ調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,12 @@ import { TagList } from '~/components/atoms/TagList';
 import { supabase } from '~/utils/supabase';
 import { useRouter } from 'next/router';
 import { User } from '@supabase/supabase-js';
+import { getRandomImageUrl } from '~/utils/demoImageUrl';
+import JobCard, {
+  JobCardProps,
+  JobStates,
+} from '~/components/molecules/JobCard';
+import { getRandomItems, programmingItems } from '~/utils/dummy';
 
 // テストでーた TODO supabaseから取得するようにする
 const options = [
@@ -21,11 +27,58 @@ const options = [
   { id: 5, name: 'Option 6' },
 ];
 
+const tagList1 = [
+  { id: 0, name: 'React' },
+  { id: 1, name: 'Next.js' },
+  { id: 2, name: 'TypeScript' },
+];
+
+const tagList2 = [
+  { id: 3, name: 'JavaScript' },
+  { id: 4, name: 'CSS' },
+  { id: 5, name: 'HTML' },
+];
+
 const Home: NextPage = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [jobData, setJobData] = useState<JobCardProps[]>([]);
 
   useEffect(() => {
     setUser(supabase.auth.user());
+    const jobData = [
+      {
+        title: 'Job Title 1',
+        imageUrl: getRandomImageUrl(),
+        status: JobStates.OPEN,
+        tagList: getRandomItems(programmingItems),
+      },
+      {
+        title: 'Job Title 2',
+        imageUrl: getRandomImageUrl(),
+        status: JobStates.CLOSED,
+        tagList: getRandomItems(programmingItems),
+      },
+      {
+        title: 'Job Title 3',
+        imageUrl: getRandomImageUrl(),
+        status: JobStates.CLOSED,
+        tagList: getRandomItems(programmingItems),
+      },
+      {
+        title: 'Job Title 4',
+        imageUrl: getRandomImageUrl(),
+        status: JobStates.CLOSED,
+        tagList: getRandomItems(programmingItems),
+      },
+      {
+        title: 'Job Title 5',
+        imageUrl: getRandomImageUrl(),
+        status: JobStates.CLOSED,
+        tagList: getRandomItems(programmingItems),
+      },
+      // ... more jobs
+    ];
+    setJobData(jobData);
   }, []);
   const signOut = () => {
     supabase.auth.signOut();
@@ -57,6 +110,17 @@ const Home: NextPage = () => {
       <div>
         <p>TabList</p>
         <TagList tagList={options} />
+      </div>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {jobData.map((job, index) => (
+          <JobCard
+            key={index}
+            title={job.title}
+            imageUrl={job.imageUrl}
+            status={job.status}
+            tagList={job.tagList}
+          />
+        ))}
       </div>
     </Layout>
   );
