36d26e57e313d0bdfb3e61ab595a829124dba4a5
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-03-26 10:11:38 +0900

    fix: レイアウト調整

--- components/atoms/TagList.tsx
+++ components/atoms/TagList.tsx
@@ -12,7 +12,7 @@ export const TagList: FC<TabListProps> = ({ tagList }) => {
     <ul className="flex flex-wrap gap-2">
       {tagList.map((tag) => (
         <li className="flex items-center" key={tag.id}>
-          <span className="ml-2 rounded-3xl border border-blue-500/100 px-1 py-1 text-sm text-blue-500">
+          <span className="ml-2 rounded-3xl border border-blue-500/100 px-1.5 py-0.5 text-xs text-blue-500">
             {tag.name}
           </span>
         </li>
